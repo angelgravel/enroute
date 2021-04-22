00f7051e4e3cf383b2ca43f23f318f25d69a9c68
VLLindqvist <victor.lindqvist@outlook.com>
2021-04-22 18:07:31 +0200

    rm me

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,43 +1,40 @@
 import React, { FC, useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
 import StoreWithProvider from "./redux/store";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import styled from "styled-components";
 
 import Home from "./routes/Home";
 import GameLounge from "./routes/GameLounge";
+import GameRoute from "./routes/GameRoute";
 
 const ENDPOINT = "http://localhost:3001";
 
 const theme = createMuiTheme({
-  shape: { 
+  shape: {
     borderRadius: 3,
   },
   palette: {
     primary: {
-      main: '#fde77d',
+      main: "#fde77d",
     },
     secondary: {
-      main: '#f9b1cd',
+      main: "#f9b1cd",
     },
     error: {
-      main: '#ff0000',
+      main: "#ff0000",
     },
   },
-  overrides: { 
+  overrides: {
     MuiButton: {
       contained: {
-        boxShadow: 'none',
+        boxShadow: "none",
         border: "4px solid",
-        margin: '10px',
-        color: 'white',
-        '& > .MuiButton-label': {
-          color: 'white', 
+        margin: "10px",
+        color: "white",
+        "& > .MuiButton-label": {
+          color: "white",
         },
       },
       containedPrimary: {
@@ -45,33 +42,33 @@ const theme = createMuiTheme({
       },
       containedSecondary: {
         borderColor: "#db93a5",
-      }
+      },
     },
     MuiTypography: {
-      h1: { 
-        fontFamily: 'Amatic SC',
-        color: "rgb(88, 88, 88)"
+      h1: {
+        fontFamily: "Amatic SC",
+        color: "rgb(88, 88, 88)",
       },
-      h2: { 
-        fontFamily: 'Amatic SC',
-        color: "rgb(88, 88, 88)"
+      h2: {
+        fontFamily: "Amatic SC",
+        color: "rgb(88, 88, 88)",
       },
-      h3: { 
-        fontFamily: 'Amatic SC',
-        color: "rgb(88, 88, 88)"
-      }
-    }
-  }
+      h3: {
+        fontFamily: "Amatic SC",
+        color: "rgb(88, 88, 88)",
+      },
+    },
+  },
 });
 
 const Container = styled.div`
-    position:fixed;
-    padding:0;
-    margin:0;
-    top:0;
-    left:0;
-    width: 100%;
-    height: 100%;
+  position: fixed;
+  padding: 0;
+  margin: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 const App: FC = () => {
@@ -88,13 +85,14 @@ const App: FC = () => {
     <StoreWithProvider>
       {/* <p>{response}</p> */}
       <Router>
-      <ThemeProvider theme={theme}>
-        <Container>
-          <Switch>
-          <Route path={`/`} exact render={() => <Home />} />
-          <Route path={`/gamelounge`} exact render={() => <GameLounge />} />
-          </Switch>
-        </Container>
+        <ThemeProvider theme={theme}>
+          <Container>
+            <Switch>
+              <Route path={`/`} exact render={() => <Home />} />
+              <Route path={`/gamelounge`} exact render={() => <GameLounge />} />
+              <Route path={`/game`} exact render={() => <GameRoute />} />
+            </Switch>
+          </Container>
         </ThemeProvider>
       </Router>
     </StoreWithProvider>
---
+++ frontend/src/routes/GameRoute.tsx
@@ -0,0 +1,14 @@
+// import {  } from "react";
+
+import styled from "styled-components";
+
+const GameRouteWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const GameRoute = () => {
+  return <GameRouteWrapper>HEJ</GameRouteWrapper>;
+};
+
+export default GameRoute;
