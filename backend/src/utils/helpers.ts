ea0fa5a781de0eeae4b550625e18498f80c3fcf3
VLLindqvist <victor.lindqvist@outlook.com>
2021-05-25 17:35:27 +0200

    Snackbar overhaul

--- backend/src/utils/helpers.ts
+++ backend/src/utils/helpers.ts
@@ -1,3 +1,6 @@
+import { Route } from "@typeDef/types";
+import cities from "../game/cities";
+
 //Fisher-Yates shuffle algorithm
 export const shuffleArray = (array: any[]): any[] => {
   let shuffledArray: any[] = Array.from(array);
@@ -7,4 +10,30 @@ export const shuffleArray = (array: any[]): any[] => {
   }
 
   return shuffledArray;
+};
+
+export const firstCap = (word: any): string => {
+  try {
+    if (typeof word !== "string" || word.length === 0) {
+      throw new Error("Not a word");
+    }
+    return word
+      .split("_")
+      .map((str: string) => str.charAt(0).toUpperCase() + str.slice(1))
+      .join(" ");
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
+};
+
+export const routeToCities = (route: Route) => {
+  const citiesIds = route
+    .split("_")
+    .map((cityId) => (cityId === "leha" ? "le_ha" : cityId));
+  return cities
+    .filter(
+      (city) => city.startsWith(citiesIds[0]) || city.startsWith(citiesIds[1]),
+    )
+    .map((city) => firstCap(city));
 };
